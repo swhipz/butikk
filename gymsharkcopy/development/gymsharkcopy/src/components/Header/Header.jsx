a8bf9d8dfe7940d77ef5d5a11f47154898d4fcb3
swhipz <97738622+swhipz@users.noreply.github.com>
2025-01-02 15:22:40 +0100

    spacer-bars

--- gymsharkcopy/development/gymsharkcopy/src/components/Header/Header.jsx
+++ gymsharkcopy/development/gymsharkcopy/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css';
 const Header = () => {
     return(
         <>
+        <div className="greybar"></div>
         <header>
         <div className="logo"><img id="logo" src="https://i.pinimg.com/originals/45/4f/47/454f470e67f2f7f59cc3b0dd37818ca8.png" alt="" /></div>
         <div className="wrap"><span className="nav">WOMEN</span><span className="nav">MEN</span><span className="nav">ACCESSORIES</span></div>
@@ -13,6 +14,7 @@ const Header = () => {
             <li><img src="https://www.svgrepo.com/show/506559/shopping-bag.svg" alt="" /></li>
         </ul>
         </header>
+        <div className="greybar" style={{height: "60px"}}></div>
         </>
     );
 }
