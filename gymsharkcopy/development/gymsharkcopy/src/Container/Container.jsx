2c2ed22b3bd17ed8d2c6a6572e2bd938c71249a4
swhipz <97738622+swhipz@users.noreply.github.com>
2025-01-02 14:47:09 +0100

    initialized react-app

---
+++ gymsharkcopy/development/gymsharkcopy/src/Container/Container.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+
+const Container = () => {
+    return(
+    <>
+    <h1>hei</h1>
+    <Header />
+    
+    </>
+    );
+
+}
+export default Container;
---
+++ gymsharkcopy/development/gymsharkcopy/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css';
+
+const Header = () => {
+    return(
+        <>
+        <h1>hei på deg</h1>
+        </>
+    );
+}
+export default Header;
