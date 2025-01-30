58e89f001d1d8c5ba890626e87ac7bace2ddcacd
swhipz <97738622+swhipz@users.noreply.github.com>
2025-01-30 22:57:46 +0100

    converted to react

---
+++ layoutSample/layout-sample/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import { Link, Route, BrowserRouter } from 'react-router';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ layoutSample/layout-sample/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+
+
+const Header = () => {
+    return(
+        <>
+        <header>
+            <div className="header-logo">
+                itemmbrowse
+            </div>
+            <ul className="header-ul">
+                <li>
+                    <Link></Link>
+                </li>
+                <li>
+                    <Link></Link>
+                </li>
+                <li>
+                    <Link></Link>
+                </li>
+            </ul>
+        </header>
+        </>
+    )
+}
+export default Header;
