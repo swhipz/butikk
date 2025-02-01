deb7e49e1b3c6b971cd12ec19d888faf144b1e51
swhipz <97738622+swhipz@users.noreply.github.com>
2025-02-01 15:39:12 +0100

    main

--- layoutSample/layout-sample/src/App.jsx
+++ layoutSample/layout-sample/src/App.jsx
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import { Link, Route, BrowserRouter } from 'react-router';
+import Home from './pages/home/Home';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
--- layoutSample/layout-sample/src/pages/home/Home.jsx
+++ layoutSample/layout-sample/src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
+import './home.css';
+
 const Home = () => {
     return(
-        <>
+    <>
+      <div className="home-container">
 
-        </>
+          <div className="home-main">
+           <div className="home-main-content">
+            <h2>Home</h2>
+            <h3>Add new meta-items</h3>
+            
+            
+           </div>
+          </div>
+        </div>
+
+    </>
     )
 }
 export default Home;
