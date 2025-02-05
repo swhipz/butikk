92bcba0dbef0a237ee6b3082ff8d2054f147af59
swhipz <97738622+swhipz@users.noreply.github.com>
2025-02-05 20:59:45 +0100

    added link

---
+++ crud_one/crud_one/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import array from './components/array';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home"
+
+
+// https://www.geeksforgeeks.org/how-to-do-crud-operations-in-reactjs/
+function App() {
+  return (
+    <div className="App">
+      <h1>Crud app 1 lols</h1>
+      <div className="crud-input-wrapper">
+        <input type="text" name="nameInput" id="nameInput" placeholder='Enter your name' style={{width: "10rem", textAlign:"center"}}/>
+        <input type="number" name="ageInput" id="ageInput" placeholder='Age' style={{width: "5rem", textAlign:"center"}}/>
+        <button>Submit</button>
+      </div>
+      <div className="crud-output-wrapper">
+        <div className="content-display">
+        
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
