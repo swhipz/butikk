19a984f623a1212100433165a228b4ae3238699c
swhipz <fredrik.jensen14@live.no>
2025-02-06 18:35:29 +0100

    something aint right

--- crud_one/crud_one/src/components/Create.js
+++ crud_one/crud_one/src/components/Create.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
-import {Form, Button} from "react-bootstrap";
+import {Form, Button, FormControl} from "react-bootstrap";
 import array from "./array";
 import {v4 as uuid} from "uuid";
 
 
 
 const Create = () => {
+ 
+
+  const addButton = document.querySelector("#add-Button");
 
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
@@ -14,13 +17,32 @@ const Create = () => {
   const history = useNavigate();
 
   const handleClickSubmitEvent = (e) => { //e stands for event, which we will call in the onClick
-      
-    e.preventDefault(); //prevents reloading as a default
-
-    const ids = uuid(); //idk what this does
+    console.log('add click detected')
     
 
+    e.preventDefault(); //prevents reloading as a default
 
+
+
+    const ids = uuid(); //idk what this does, tutorial says it makes a unique id
+    let uni = ids.slice(0,8); //slices (copies) the indexed numbers
+
+    // now we need to get the value that is inside usestate and push that to the JS object
+
+    let a = name,
+        b = age;
+      
+    //make sure /check if usestate is empty
+
+    if (name === "" || age === ""){
+      return ("Invalid input, or empty");
+    }
+    //this pushes the new shit to the array
+    array.push({ id: uni, Name: a, Age: b});
+    console.log("push complete");
+
+    //return back to homepage after successful push
+    history("/..");
   }
 
 
@@ -30,19 +52,29 @@ const Create = () => {
         <div className="crud-wrapper" style={{display: "flex", flexDirection:"column"}}>
             <h2>Create new user!</h2>
         <div className="crud-input-wrapper" style={{width:"100%", height:"100%", padding:"1rem", display:"flex", gap:".5rem", alignItems:"center", justifyContent:"center"}}>
-        <input type="text" name="nameInput" id="nameInput" placeholder='Enter your name' style={{textAlign:"center", margin:"0", height: "2.25rem"}}/>
-
+        <Form className="d-grid gap-2" style={{width:"100%", height:"100%", padding:"1rem", display:"flex", gap:".5rem", alignItems:"center", justifyContent:"center"}}>
+          <Form.Group>
+            <FormControl onChange={(e)=> {
+              setName(e.target.value)
+            }}
+            type="text"
+            placeholder="Enter name"
+            required
+            />
+          </Form.Group>
         <input type="number" name="ageInput" id="ageInput" placeholder='Age' style={{textAlign:"center",  height: "2.25rem"}} />
-        <select name="typeInput" id="typeInput" placeholder="Type" style={{ height: "2.25rem"}}>
+        <select name="typeInput" id="typeInput" placeholder="Type" style={{ height: "2.25rem", textAlign:"center"}}>
           <option value="Humanoid">Humanoid</option>
           <option value="Kanine">Kanine</option>
           <option value="other">Other</option>
         </select>
-
-        <Button onClick={()=>{console.log('balls')}} variant="outline-primary" style={{position:"relative"}}>Add</Button>
+        <Button id="add-Button" onClick={(e)=>{handleClickSubmitEvent(e)}} variant="primary" style={{position:"relative"}}>Add</Button>
+       </Form>
         </div>
       </div>
         <Link to="/.."><Button style={{margin: "1rem", width:"100%"}}>Back to overview</Button></Link>
+
+
 </>
     )
     
